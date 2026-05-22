0138a2cecd0305f4a1a9b60d7ddd9026fcd62517
Aldrin-DP <aldrinpelayo.adp@gmail.com>
2026-05-22 09:51:01 +0800

    Implement header and nav bar design

--- resources/js/Layouts/Partials/TheHeader.vue
+++ resources/js/Layouts/Partials/TheHeader.vue
@@ -1,47 +1,67 @@
 <template>
-    <div class="bg-gray-950 text-gray-400 flex items-center h-20">
-        <header class="w-full h-full mx-20 flex justify-between items-center">
-            <div class="flex items-center gap-1">
-                <Link href="/">
-                    <img src="/public/images/sea.png" width="55px" alt="logo" class="hover:scale-115 transition-all duration-300">
-                </Link>
-                <h2 class="font-bold text-2xl">Sea Anime</h2>
+    <div class="bg-gray-950 text-gray-300 flex items-center">
+        <header class="max-w-325 w-full lg:flex lg:mx-5 xl:mx-20 lg:items-center">
+            <div class="flex h-20 justify-between items-center w-full px-3 lg:w-auto lg:px-0">
+                <div class="flex items-center gap-1">
+                    <Link href="/">
+                        <img src="/public/images/sea.png" width="55px" alt="logo" class="hover:scale-115 transition-all duration-300">
+                    </Link>
+                    <h2 class="font-bold text-xl lg:text-2xl">Sea Anime</h2>
+                </div>
+                <!-- Hamburger -->
+                 <div class="lg:hidden flex justify-between items-center gap-5">
+                    <div class="flex items-center gap-0.5 text-[15px] font-semibold px-3 py-1.5 border border-transparent hover:text-gray-100 hover:border-gray-700 hover:rounded hover:shadow hover:shadow-blue-950 transition-all duration-300">
+                        <MoonIcon class="size-3.5"/>
+                        <button>Abyss</button>
+                    </div>
+                    <button @click="toggleNav" class="lg:hidden cursor-pointer border border-transparent  px-1 py-1 hover:border-gray-800 hover:text-gray-100 hover:shadow-lg hover:shadow-gray-900 transition-all duration-300">
+                        <XMarkIcon v-if="isOpen" class="size-6.5" />
+                        <Bars3Icon v-else class="size-6.5" />
+                    </button>
+                 </div>
             </div>
 
-            <div tabindex="0"
-                class="flex items-center gap-2 border px-3 py-1 rounded-full border-gray-700 focus-within:outline-2 focus-within:outline-blue-700">
-                <input
-                    type="text"
-                    class="outline:none focus:border-none focus:outline-0"
-                    placeholder="Search anime..."
+            <div  :class="isOpen ? 'flex' : 'hidden'" class="lg:flex flex-col justify-between absolute w-full lg:w-auto px-3 lg:px-0 py-4 lg:py-0 border-t border-gray-800 lg:flex-1 bg-gray-950 h-auto z-20 lg:flex-row lg:relative lg:z-0 lg:border-none">
+                <div
+                    tabindex="0"
+                    class="flex items-center mb-3 lg:mb-0 lg:mx-5 xl:mx-10 gap-2 border px-3 py-2 lg:py-1 rounded-full border-gray-700 focus-within:outline-2 focus-within:outline-blue-700"
                 >
-                <MagnifyingGlassIcon class="size-5"/>
-            </div>
-
-            <div class="flex items-center gap-5">
-                <NavLink href="/"> Surface </NavLink>
-                <NavLink href="/"> Explore the Deep </NavLink>
-            </div>
-
-            <div class="flex items-center gap-3">
-                <div class="flex items-center gap-0.5 text-[15px] font-semibold px-3 py-1.5 border border-transparent hover:text-gray-100 hover:border-gray-700 hover:rounded hover:shadow hover:shadow-blue-950 transition-all duration-300">
-                    <MoonIcon class="size-4"/>
-                    <button>Abyss</button>
-                </div>
-
-                <NavLink href="/" class="flex items-center gap-px">
-                    <ArrowRightEndOnRectangleIcon class="size-5" />
-                    Log in
-                </NavLink>
-                <div>
-                    <BaseButton
-                        variant="primary"
-                        disabled
+                    <input
+                        type="text"
+                        class="w-full lg:w-auto outline:none focus:border-none focus:outline-0"
+                        placeholder="Search anime..."
                     >
-                        Join the Deep
-                    </BaseButton>
+                    <MagnifyingGlassIcon class="size-5"/>
+                </div>
+
+                <div class="flex flex-col flex-1 justify-between lg:flex-row lg:items-center">
+                    <div class="flex flex-col items-center lg:flex-row lg:gap-2">
+                        <NavLink class="w-full lg:w-auto" href="/"> Surface </NavLink>
+                        <NavLink class="w-full lg:w-auto" href="/"> Explore the Deep</NavLink>
+                    </div>
+
+                    <div class="flex flex-col items-center lg:justify-between gap-2 mt-4 lg:mt-0 pt-3 lg:pt-0 border-t border-gray-800 lg:flex-row lg:border-none">
+                        <div class="hidden lg:flex items-center gap-1 text-[15px] font-semibold px-3 py-1.5 border border-transparent hover:text-gray-100 hover:border-gray-700 hover:rounded hover:shadow hover:shadow-blue-950 transition-all duration-300">
+                            <MoonIcon class="size-3.5"/>
+                            <button>Abyss</button>
+                        </div>
+
+                        <NavLink href="/" class="flex items-center justify-center w-full lg:w-auto gap-1">
+                            <ArrowRightEndOnRectangleIcon class="size-5" />
+                            Log in
+                        </NavLink>
+                        <div class="w-full lg:w-auto">
+                            <BaseButton
+                                variant="primary"
+                                class="w-full lg:w-auto"
+                            >
+                                Sign up
+                            </BaseButton>
+                        </div>
+                    </div>
                 </div>
             </div>
+
         </header>
     </div>
 </template>
@@ -49,7 +69,7 @@
 <script>
 import BaseButton from '../../Components/Base/BaseButton.vue';
 import NavLink from '../../Components/NavLink.vue';
-import { MagnifyingGlassIcon, SunIcon, MoonIcon, ArrowRightEndOnRectangleIcon } from '@heroicons/vue/24/solid';
+import { MagnifyingGlassIcon, SunIcon, MoonIcon, ArrowRightEndOnRectangleIcon, Bars3Icon, XMarkIcon } from '@heroicons/vue/24/solid';
 
 
 export default {
@@ -59,11 +79,18 @@ export default {
         MagnifyingGlassIcon,
         SunIcon,
         MoonIcon,
-        ArrowRightEndOnRectangleIcon
+        ArrowRightEndOnRectangleIcon,
+        Bars3Icon,
+        XMarkIcon
     },
     data() {
         return {
-
+            isOpen: false
+        }
+    },
+    methods: {
+        toggleNav() {
+            this.isOpen = !this.isOpen;
         }
     }
 }
