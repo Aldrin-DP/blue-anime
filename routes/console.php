af72fcab84fb92ab31ace38210f4151d28403e28
Aldrin <aldrinpelayo.adp@gmail.com>
2026-08-12 18:23:55 +0800

    change timezone and add schedule commands

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::command('app:sync-popular-anime')->dailyAt('03:00');
+Schedule::command('app:sync-popular-chinese-anime')->dailyAt('03:30');
+Schedule::command('app:sync-seasonal-anime')->dailyAt('04:00');
+Schedule::command('app:sync-top-rated-anime')->dailyAt('04:30');
+Schedule::command('app:sync-trending-anime')->dailyAt('05:00');
+Schedule::command('app:sync-trending-chinese-anime')->dailyAt('05:30');
